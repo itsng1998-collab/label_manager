8f2ec0a488eaf0ad9ed394fbee61e38a3e9114da
김황득 <garang@innowireless.com>
2026-07-29 18:17:25 +0900

    라벨 이미지 import model을 application으로 이동

--- lib/features/label_sheet/label_sheet_open_xml_export.dart
+++ lib/features/label_sheet/label_sheet_open_xml_export.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 
 import 'package:archive/archive.dart';
 import 'package:fortune_sheet/fortune_sheet.dart';
-import 'package:label_manager/features/label_sheet/label_sheet_import_model.dart';
+import 'package:label_manager/features/label_sheet/application/label_sheet_import_model.dart';
 
 const String labelSheetOpenXmlTestPath = '.tmp/test.xlsx';
 const String labelSheetOpenXmlFallbackTestPath = '.tmp/test_latest.xlsx';
