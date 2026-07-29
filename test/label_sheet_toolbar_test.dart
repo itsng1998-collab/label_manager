5dacc2f3487a6157b0c04e3b6df68a3ef754f7d3
김황득 <garang@innowireless.com>
2026-07-29 18:07:19 +0900

    라벨 시트 AI import를 application으로 이동

--- test/label_sheet_toolbar_test.dart
+++ test/label_sheet_toolbar_test.dart
@@ -24,7 +24,7 @@ import 'package:label_manager/features/item/domain/item.dart';
 import 'package:label_manager/features/item/domain/item_of_market.dart';
 import 'package:label_manager/features/label_size/domain/label_size.dart';
 import 'package:label_manager/widgets/preview_floating_window.dart';
-import 'package:label_manager/features/label_sheet/label_sheet_ai_import.dart';
+import 'package:label_manager/features/label_sheet/application/label_sheet_ai_import.dart';
 import 'package:label_manager/features/label_sheet/label_sheet_ai_import_temp.dart';
 import 'package:label_manager/features/label_sheet/domain/label_sheet_required_keyword.dart';
 import 'package:label_manager/features/label_sheet/presentation/label_sheet_page.dart';
