19862d9ed06f2cb71e5674cae23c797712ad04bc
김황득 <garang@innowireless.com>
2026-07-29 17:56:29 +0900

    라벨 시트 page를 presentation으로 이동

--- test/label_sheet_toolbar_test.dart
+++ test/label_sheet_toolbar_test.dart
@@ -27,7 +27,7 @@ import 'package:label_manager/widgets/preview_floating_window.dart';
 import 'package:label_manager/features/label_sheet/label_sheet_ai_import.dart';
 import 'package:label_manager/features/label_sheet/label_sheet_ai_import_temp.dart';
 import 'package:label_manager/features/label_sheet/domain/label_sheet_required_keyword.dart';
-import 'package:label_manager/features/label_sheet/label_sheet_page.dart';
+import 'package:label_manager/features/label_sheet/presentation/label_sheet_page.dart';
 import 'package:label_manager/features/label_sheet/label_sheet_import_model.dart';
 import 'package:label_manager/features/label_sheet/label_sheet_native_open_xml.dart';
 import 'package:label_manager/features/label_sheet/label_sheet_open_xml_export.dart';
